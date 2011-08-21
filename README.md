505de359118e55ccb199a4355387d1d42c0b1242
Alexandre Mennelet <amennelet@yahoo.fr>
2011-08-21 21:28:12 +0200

    starting the webserver and couchdb connection

--- README.md
+++ README.md
@@ -4,16 +4,29 @@
 This is a [nodejs](http://nodejs.org) web server to edit [couchdb](http://couchdb.apache.org/) _design/document directly into your browser with the [ace](https://github.com/ajaxorg/ace) editor.
 
 ## Installation
-1. Get the couchapp-editor.
+1. First you need a [CouchDB](http://couchdb.apache.org/) server up and running and [NodeJS](http://nodejs.org) and [npm](http://search.npmjs.org/#/_install) installed.
+
+2. Get the couchapp-editor.
 
         git clone git://github.com/amennelet/couchapp-editor.git
         cd ./couchapp-editor
-2. Download the latest [ace editor](https://github.com/ajaxorg/ace)
 
-        curl -L -O -C - https://github.com/downloads/ajaxorg/ace/ace-0.2.0.zip
-3. Unzip it
+3. Download the latest [ace editor](https://github.com/ajaxorg/ace)
 
-        unzip ace-0.2.0.zip
-4. Start the server
+        curl -L -O -C - https://github.com/downloads/ajaxorg/ace/ace-xxxxxxx.zip
+
+4. Unzip it
+
+        unzip ace-xxxxxxx.zip
+
+5. Link the ace directory to ace
+
+        ln -s ace-xxxxxxx ace
+
+6. Get the last [couchdb-api](https://github.com/dominicbarnes/node-couchdb-api)
+
+        npm install couchdb-api
+
+7. Start the server
 
         ./couchapp-server.js
--- couchapp-server.js
+++ couchapp-server.js
@@ -2,9 +2,67 @@
 
 var http = require('http')
 , serverPort = 8765
+, couchClient = require('couchdb-api')
+, url = require('url')
+, pathSplitRegex = new RegExp('/')
+, hostRegex = new RegExp(':')
 ;
 
+function getServerDesign(uri){
+    if(uri.lenght == 0 || uri == '/') uri = '/localhost';
+    var paths = uri.split(pathSplitRegex);
+    var serverDesign = {};
+    if(paths.length > 1 && paths[1].length > 0){
+	serverDesign.server = {};
+	var host = paths[1].split(hostRegex);
+	if(host.lenght == 1){
+	    serverDesign.server.host = host[0];
+	    serverDesign.server.port = 5984;	    
+	}
+	else if(host.lenght == 2){
+	    serverDesign.server.host = host[0];
+	    serverDesign.server.port = parseInt(host[1]);	    
+	}
+	else{
+	    serverDesign.server.host = 'localhost';
+	    serverDesign.server.port = 5984;
+	}
+    }
+    if(paths.length > 2 && paths[2].length > 0) serverDesign.database = paths[2];
+    if(paths.length > 3 && paths[3].length > 0) serverDesign.design = paths[3];
+    return serverDesign;
+}
+
 http.createServer(function(request, response){
+    var serverDesign = getServerDesign(url.parse(request.url).pathname);
+
+    if(!serverDesign.server){
+	serverDesign.server.host = 'localhost';
+	serverDesign.server.port = 5984;
+    }
+
+    if(serverDesign.design){
+	// get the design
+	// edit the design
+	response.write('database:' + serverDesign.database);
+	response.write('\ndesign:' + serverDesign.design);
+	response.end();
+    }
+    else if(serverDesign.database){
+	response.write('database:' + serverDesign.database);
+	response.end();
+    }
+    else{
+	// get the list of server design
+	var server = couchClient.srv(serverDesign.server.host, serverDesign.server.port);
+	server.allDbs(function(err, allDbs){
+	    if(err)response.write("Error:" + err);
+	    else{
+		response.write(JSON.stringify(allDbs));		
+	    }
+	    response.end();
+	});
+    }
 }).listen(serverPort, "0.0.0.0");
 
 console.log("couchapp server running : http://localhost:" + serverPort);
